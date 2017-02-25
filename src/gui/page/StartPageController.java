210ac0608bd4692f2d3f434e16c87133c216bb1f
Eimantas <eimantas98@gmail.com>
2017-02-25 16:42:36 +0100

    Start page.

---
+++ src/gui/page/StartPageController.java
@@ -0,0 +1,27 @@
+package gui.page;
+
+import gui.PageController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class StartPageController {
+
+    @FXML
+    private Button buttonHoS, buttonDVIR;
+
+    private PageController pageController = PageController.getInstance();
+
+    @FXML
+    private void initialize(){
+    }
+
+    @FXML
+    private void buttonHoSClicked(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void buttonDVIRClicked(ActionEvent actionEvent) {
+    }
+}
